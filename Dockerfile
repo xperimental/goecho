932531b15ce41d0a90973e90036b260964238d2e
Robert Jacob <robert.jacob@holidaycheck.com>
2017-07-02 16:12:11 +0200

    Use multi-stage build in Dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
+FROM golang:1.8.3-onbuild AS builder
+
 FROM scratch
 MAINTAINER Robert Jacob <robert.jacob@holidaycheck.com>
 EXPOSE 8080
 
-ADD goecho /goecho
+COPY --from=builder /go/bin/goecho /goecho
 CMD ["/goecho"]
--- build.sh
+++ build.sh
@@ -6,10 +6,5 @@ set -o pipefail
 
 source version.sh
 
-echo "Build static executable..."
-readonly LD_FLAGS="-w -X main.Version=${VERSION}"
-
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags "${LD_FLAGS}" -o "goecho" "github.com/xperimental/goecho"
-
 echo "Build docker container..."
 docker build -t "${IMAGE_NAME}" .
