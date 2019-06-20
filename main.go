04c3e8d624147ac5d4aecc3ccf0b954ca33f8c4d
Robert Jacob <robert.jacob@holidaycheck.com>
2019-06-20 13:00:20 +0200

    Implement graceful shutdown logic.

--- main.go
+++ main.go
@@ -1,18 +1,27 @@
 package main // import "github.com/xperimental/goecho"
 
 import (
+	"context"
 	"flag"
 	"log"
+	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
-// Version is set to the build version when building using the build script.
-var Version = "unknown"
+var (
+	// Version is set to the build version when building using the build script.
+	Version = "unknown"
 
-var addr string
+	addr          = ":8080"
+	gracefulDelay = 2 * time.Second
+)
 
 func main() {
-	flag.StringVar(&addr, "addr", ":8080", "Address and port to listen on.")
+	flag.StringVar(&addr, "addr", addr, "Address and port to listen on.")
+	flag.DurationVar(&gracefulDelay, "graceful-delay", gracefulDelay, "Delay between receiving a shutdown signal and starting shutdown.")
 	flag.Parse()
 
 	hostname, err := os.Hostname()
@@ -24,6 +33,30 @@ func main() {
 
 	server := createServer(addr, Version, hostname, env)
 
+	shutdownErrCh := make(chan error)
+	go func() {
+		sigCh := make(chan os.Signal)
+		signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+
+		<-sigCh
+		signal.Reset()
+
+		if gracefulDelay > 0 {
+			log.Printf("Waiting %s for shutdown...", gracefulDelay)
+			time.Sleep(gracefulDelay)
+		}
+
+		log.Println("Shutting down...")
+		shutdownErrCh <- server.Shutdown(context.Background())
+	}()
+
 	log.Printf("Listening on %s\n", addr)
-	log.Fatal(server.ListenAndServe())
+	err = server.ListenAndServe()
+	if err != nil && err != http.ErrServerClosed {
+		log.Fatalf("Error during listen: %s", err)
+	}
+
+	if err := <-shutdownErrCh; err != nil {
+		log.Fatalf("Error during shutdown: %s", err)
+	}
 }
